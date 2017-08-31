a165cbccb2820ad15657236b536cd77a3a720ea7
ntwcklng <ntwcklng@gmail.com>
2017-08-31 19:17:02 +0200

    fix footer

--- components/socialitem.js
+++ components/socialitem.js
@@ -6,9 +6,13 @@ text-align: center;
 color: #c1c1c1;
 padding: 30px;
 text-decoration: none;
+display: inline-block;
 &:hover {
   color: #50A8DB;
-}
+};
+@media (max-width: 713px) {
+  padding: 10px; 
+};
 `
 export default({text, link}) => (
   <Item href={link} target="_blank">{text}</Item>
