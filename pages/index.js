562741daf32ba0771eb9c5088f005a0ce67ce277
ntwcklng <ntwcklng@gmail.com>
2017-09-04 08:05:58 +0200

    video

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,6 @@ import Video from '../components/video'
 export default () => (
   <Page>
     <Heading>Willkommen bei Autopflege Mieth</Heading>
-    <Video link="https://www.youtube.com/embed/tLG_XRvTMMI" />
+    <Video link="https://www.youtube.com/embed/hkeu1rog2CY" />
   </Page>
 )
