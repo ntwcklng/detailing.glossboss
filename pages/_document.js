071407be27727a5bb1e0fa8d349ca8910cca933f
ntwcklng <ntwcklng@gmail.com>
2018-05-21 18:32:05 +0200

    remove google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -27,17 +27,6 @@ export default class MyDocument extends Document {
     return (
       <html lang="de">
         <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-20363654-2', 'auto');
-            ga('send', 'pageview');`
-            }}
-          />
           <title>{config.title}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content={config.mainColor} />
