edf114d16aeb6ba37733d05f9b267af10639afc7
ntwcklng <ntwcklng@gmail.com>
2020-11-21 16:55:27 +0100

    ?

--- pages/404.js
+++ pages/404.js
@@ -2,27 +2,13 @@ import Page from '../layouts/page'
 import Heading from '../components/heading'
 
 import React from 'react'
-export default class Error extends React.Component {
-  static getInitialProps({ res, xhr }) {
-    const statusCode = res ? res.statusCode : xhr ? xhr.status : null
-    return { statusCode }
-  }
-
-  render() {
-    return (
+const MissingComponentPage = () => (
       <Page>
-        {this.props.statusCode === 404 ? (
           <div>
             <Heading>404</Heading>
-            <p>Page could not be found.</p>
+            <p>Diese Seite existiert nicht.</p>
           </div>
-        ) : (
-          <div>
-            <Heading>Server Error</Heading>
-            <p>{this.props.statusCode}</p>
-          </div>
-        )}
       </Page>
-    )
-  }
-}
+)
+export default MissingComponentPage
+
